49aa659a48835fab4411e9625a15b4049146e6db
Ritanshi-arch <112924759+Ritanshi-arch@users.noreply.github.com>
2022-11-06 18:32:54 +0530

    Change

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
+// selecting all inputs from HTML world
+
 var initialPrice = document.querySelector("#initial-price");
-var stocksQuantity = document.querySelector("#stock-quantity");
+var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
-submitBtn.addEventListener('click', submitHandler)
 
 
+submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler() {
     var ip = Number(initialPrice.value);
     var qty = Number(stocksQuantity.value);
     var curr = Number(currentPrice.value);
-  calculateProfitAndLoss(ip, qty, curr);
+
+    if (ip && curr) {
+        if (ip <= 0 || curr <= 0 || qty <= 0) {
+            outputBox.innerText = "Negative values or Zero not allowed. Please enter only inputs greater than 0";
+            return;
+            // alert("Negative values not allowed. Please enter only inputs greater than 0");
+        }
+    } else {
+        outputBox.innerText = "Please enter valid inputs, do not enter 0 or leave any field empty";
+        return;
+        //alert("Please enter inputs. Do not leave any field blank.");
+    }
+
+    if (ip > 0 && qty > 0 && curr > 0) {
+
+        calculateProfitandLoss(ip, qty, curr);
+    }
+
 }
 
-function calculateProfitAndLoss(initial, quantity, current) {  if(initialPrice.value < 0){
-  outputBox.innerText=("😑Don't insert negative value in Initial Price😑");
-} 
-else if (stocksQuantity.value < 0 ) {
-  outputBox.innerText=("😑Don't insert negative value in Quantity of Stocks😑");
-}
- else if(currentPrice.value < 0 ){
-  outputBox.innerText=("😑Don't insert negative value in Current Price😑");
- }else if( initialPrice.value == 0){
-  outputBox.innerText=("🤨 Don't leave value of Initial price empty 🤨")
- } else if( stocksQuantity.value == 0){
-  outputBox.innerText=("🤨Don't leave value of quantity of Stock empty🤨")
- }else if( currentPrice.value == 0){
-  outputBox.innerText=("🤨Don't leave value of current price empty🤨")
- }
+// calculate profit, loss, profit percentage, loss percentage
+
+function calculateProfitandLoss(initial, quantity, current) {
 
 
-   else  if (initial > current) {
-        //loss Logic here
-        var loss = (initial - current) * quantity;
-        var lossPercentge = (loss / initial) * 100;
+    if (initial > current) {
 
-        showOutput(`🤒 Hey the loss is ${loss} and the percente is ${lossPercentge}% 🤒`);
+        // loss
+        var loss = ((initial - current) * quantity).toFixed(2);
+        var lossPercentage = (((initial - current) * 100) / initial).toFixed(2);
+        document.body.style.backgroundColor = "red";
+        outputBox.innerText = `Alas! The loss is ${loss} and loss percentage is ${lossPercentage} % 🔴`;
+
     } else if (current > initial) {
-        var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / initial) * 100; 
 
-        showOutput(`🤩 Hey the profit is ${profit} and the percentage is ${profitPercentage}% 🤩` );
+        // profit 
+        var profit = ((current - initial) * quantity).toFixed(2);
+        var profitPercentage = (((current - initial) * 100) / initial);
+        document.body.style.backgroundColor = "grey";
+        outputBox.innerText = `Awww! The profit is ${profit} and profit percentage is ${profitPercentage.toFixed(2)} % 🟢`;
 
-   } else {
-        showOutput(`No pain no gain and no gain no pain`);
+    } else {
+
+        // rest
+        outputBox.innerText = `No Pain No Gain : Raju ricks nhi lene ka re baba`;
+        // console.log("Raju ricks nhi lene ka re baba")
     }
-      function showOutput(message) {
 
-        outputBox.innerHTML = message;
-      }
-    }
+}
